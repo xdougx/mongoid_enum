c73fb9648905f0b3e480dea63d2544e524ec530b
Douglas Rossignolli <douglas@kanamobi.com.br>
2018-03-06 18:18:27 -0300

    fix

--- lib/mongoid/enum.rb
+++ lib/mongoid/enum.rb
@@ -7,135 +7,135 @@ module Mongoid # :nodoc:
     extend ActiveSupport::Concern
 
     included do
-      class_attribute(:enums)
+      class_attribute(:Ver)
       self.enums = {}
     end
 
     # :nodoc:
-    module ClassMethods
-      # Define enum field on the model. See description of Mongoid::Enum
-      def enum(definitions)
-        klass = self
-        enum_prefix = definitions.delete(:_prefix)
-        enum_suffix = definitions.delete(:_suffix)
-        default_key = definitions.delete(:_default)
-        pluralize   = definitions.delete(:_plural_scopes)
+    # module ClassMethods
+    #   # Define enum field on the model. See description of Mongoid::Enum
+    #   def enum(definitions)
+    #     klass = self
+    #     enum_prefix = definitions.delete(:_prefix)
+    #     enum_suffix = definitions.delete(:_suffix)
+    #     default_key = definitions.delete(:_default)
+    #     pluralize   = definitions.delete(:_plural_scopes)
 
-        definitions.each do |name, values|
-          enum_values = ActiveSupport::HashWithIndifferentAccess.new
-          name        = name.to_sym
-          const_name  = name.to_s.pluralize.upcase
+    #     definitions.each do |name, values|
+    #       enum_values = ActiveSupport::HashWithIndifferentAccess.new
+    #       name        = name.to_sym
+    #       const_name  = name.to_s.pluralize.upcase
 
-          if klass.const_defined?(const_name)
-            fail ArgumentError, "Defining enum :#{name} on #{klass} would " \
-              "overwrite existing constant #{klass}::#{const_name}"
-          end
+    #       if klass.const_defined?(const_name)
+    #         fail ArgumentError, "Defining enum :#{name} on #{klass} would " \
+    #           "overwrite existing constant #{klass}::#{const_name}"
+    #       end
 
-          detect_enum_conflict!(name, name)
-          detect_enum_conflict!(name, "#{name}=")
+    #       detect_enum_conflict!(name, name)
+    #       detect_enum_conflict!(name, "#{name}=")
 
-          if values.respond_to? :each_pair
-            values.each_pair { |key, value| enum_values[key.to_s] = value }
-          else
-            values.each { |v| enum_values[v.to_s] = v.to_s }
-          end
+    #       if values.respond_to? :each_pair
+    #         values.each_pair { |key, value| enum_values[key.to_s] = value }
+    #       else
+    #         values.each { |v| enum_values[v.to_s] = v.to_s }
+    #       end
 
-          enum_values.each do |key, value|
-            key.freeze
-            value.freeze
-          end
-          enum_values.freeze
+    #       enum_values.each do |key, value|
+    #         key.freeze
+    #         value.freeze
+    #       end
+    #       enum_values.freeze
 
-          if default_key && !enum_values.key?(default_key)
-            fail ArgumentError, "default key #{default_key} is not among enum options"
-          end
+    #       if default_key && !enum_values.key?(default_key)
+    #         fail ArgumentError, "default key #{default_key} is not among enum options"
+    #       end
 
-          field name, type: EnumType.new(enum_values), default: default_key
+    #       field name, type: EnumType.new(enum_values), default: default_key
 
-          klass.const_set const_name, enum_values
-          klass.validates name,
-                          inclusion: {
-                            in: enum_values.keys,
-                            allow_nil: true,
-                            message: "is invalid"
-                          }
+    #       klass.const_set const_name, enum_values
+    #       klass.validates name,
+    #                       inclusion: {
+    #                         in: enum_values.keys,
+    #                         allow_nil: true,
+    #                         message: "is invalid"
+    #                       }
 
-          _enum_methods_module.module_eval do
-            enum_values.each do |key, value|
-              if enum_prefix == true
-                prefix = "#{name}_"
-              elsif enum_prefix
-                prefix = "#{enum_prefix}_"
-              end
-              if enum_suffix == true
-                suffix = "_#{name}"
-              elsif enum_suffix
-                suffix = "_#{enum_suffix}"
-              end
+    #       _enum_methods_module.module_eval do
+    #         enum_values.each do |key, value|
+    #           if enum_prefix == true
+    #             prefix = "#{name}_"
+    #           elsif enum_prefix
+    #             prefix = "#{enum_prefix}_"
+    #           end
+    #           if enum_suffix == true
+    #             suffix = "_#{name}"
+    #           elsif enum_suffix
+    #             suffix = "_#{enum_suffix}"
+    #           end
 
-              value_method_name = "#{prefix}#{key}#{suffix}"
-              scope_name = pluralize ? value_method_name.pluralize : value_method_name
+    #           value_method_name = "#{prefix}#{key}#{suffix}"
+    #           scope_name = pluralize ? value_method_name.pluralize : value_method_name
 
-              # def active?() status == 0 end
-              klass.send(:detect_enum_conflict!, name, "#{value_method_name}?")
-              define_method("#{value_method_name}?") { self[name] == value }
+    #           # def active?() status == 0 end
+    #           klass.send(:detect_enum_conflict!, name, "#{value_method_name}?")
+    #           define_method("#{value_method_name}?") { self[name] == value }
 
-              # def active!() update! status: :active end
-              klass.send(:detect_enum_conflict!, name, "#{value_method_name}!")
-              define_method("#{value_method_name}!") { update! name => key }
+    #           # def active!() update! status: :active end
+    #           klass.send(:detect_enum_conflict!, name, "#{value_method_name}!")
+    #           define_method("#{value_method_name}!") { update! name => key }
 
-              # scope :active, -> { where status: 0 }
-              klass.send(:detect_enum_conflict!, name, scope_name, true)
-              klass.scope scope_name, -> { klass.where name => key }
-            end
-          end
+    #           # scope :active, -> { where status: 0 }
+    #           klass.send(:detect_enum_conflict!, name, scope_name, true)
+    #           klass.scope scope_name, -> { klass.where name => key }
+    #         end
+    #       end
 
-          # dup so children classes don't add their own enums to parent definitions
-          self.enums = enums.dup
+    #       # dup so children classes don't add their own enums to parent definitions
+    #       self.enums = enums.dup
 
-          enums[name] = enum_values
-          enums.freeze
-        end
-      end
+    #       enums[name] = enum_values
+    #       enums.freeze
+    #     end
+    #   end
 
-      private
+    #   private
 
-      def _enum_methods_module
-        @_enum_methods_module ||= begin
-          mod = Module.new
-          include mod
-          mod
-        end
-      end
+    #   def _enum_methods_module
+    #     @_enum_methods_module ||= begin
+    #       mod = Module.new
+    #       include mod
+    #       mod
+    #     end
+    #   end
 
-      # :nodoc:
-      ENUM_CONFLICT_MESSAGE = \
-        "You tried to define an enum named \"%{enum}\" on the model \"%{klass}\", but " \
-        "this will generate %{type} method \"%{method}\", which is already defined."
+    #   # :nodoc:
+    #   ENUM_CONFLICT_MESSAGE = \
+    #     "You tried to define an enum named \"%{enum}\" on the model \"%{klass}\", but " \
+    #     "this will generate %{type} method \"%{method}\", which is already defined."
 
-      def detect_enum_conflict!(enum_name, method_name, class_method = false)
-        method_name = method_name.to_sym
+    #   def detect_enum_conflict!(enum_name, method_name, class_method = false)
+    #     method_name = method_name.to_sym
 
-        if class_method
-          if self.respond_to?(method_name, true)
-            raise_conflict_error(enum_name, method_name, "class")
-          end
-        else
-          if Mongoid.destructive_fields.include?(method_name) ||
-             instance_methods.include?(method_name)
-            raise_conflict_error(enum_name, method_name, "instance")
-          end
-        end
-      end
+    #     if class_method
+    #       if self.respond_to?(method_name, true)
+    #         raise_conflict_error(enum_name, method_name, "class")
+    #       end
+    #     else
+    #       if Mongoid.destructive_fields.include?(method_name) ||
+    #          instance_methods.include?(method_name)
+    #         raise_conflict_error(enum_name, method_name, "instance")
+    #       end
+    #     end
+    #   end
 
-      def raise_conflict_error(enum_name, method_name, type)
-        fail ArgumentError, ENUM_CONFLICT_MESSAGE % {
-          enum: enum_name,
-          klass: name,
-          type: type,
-          method: method_name
-        }
-      end
-    end
+    #   def raise_conflict_error(enum_name, method_name, type)
+    #     fail ArgumentError, ENUM_CONFLICT_MESSAGE % {
+    #       enum: enum_name,
+    #       klass: name,
+    #       type: type,
+    #       method: method_name
+    #     }
+    #   end
+    # end
   end
 end
